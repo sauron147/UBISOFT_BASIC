7389811ca65963864ef86e69a5700ed7130f142f
sauron147 <icqloge123@gmail.com>
2025-06-05 09:23:00 +0900

    second day

---
+++ Project_E/Assets/scrup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonListener : MonoBehaviour
+{
+    public void OnButtonClicked()
+    {
+        print("버튼을 클릭했습니다.");
+    }
+}
